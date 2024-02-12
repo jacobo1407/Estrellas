613e4c1fc613af30b0464468c45597278b115aa5
brianSalguero <133128085+brianSalguero@users.noreply.github.com>
2024-02-12 15:26:24 +0100

    Update Rooms.js

--- estrellas/src/screens/Rooms.js
+++ estrellas/src/screens/Rooms.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import {
   View,
   Image,
@@ -9,24 +9,62 @@ import {
 } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { Card, Title, Paragraph } from 'react-native-paper';
-const images = [];
+
 const Rooms = (props) => {
   const [roomImages, setRoomImages] = useState([]);
+  const [id, setId] = useState(1);
+  const [description, setDescription] = useState('');
+  const [peopleNumber, setPeopleNumber] = useState(null);
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+  const getInfo = async () => {
+    try {
+      const response = await fetch(
+        `http://44.195.98.192:8080/ESTRELLAS/roomInformation?id=${id}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+
+        const images = data.images;
+        const description = data.description;
+        const numPeople = data.peopleNumber;
+        const price = data.price;
+
+        setDescription(description);
+        setPeopleNumber(numPeople);
+        setPrice(price);
+        setRoomImages(images);
+      } else {
+        Alert.alert('Error', `Failed to fetch Room`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <View style={styles.container}>
-        <Swiper style={styles.carouselWrapper} ref={swiperRef}>
-          {images.map((image, index) => (
+        <Swiper style={styles.carouselWrapper}>
+          {roomImages.map((image, index) => (
             <View style={styles.slide} key={index}>
               <Image
-                source={roomImages}
+                source={{ uri: `data:image/jpg;base64,${image}` }}
                 style={[styles.image, styles.border]}
                 resizeMode="cover"
               />
             </View>
           ))}
         </Swiper>
+        <Text> Description </Text>
+        <Text>{description} </Text>
+        <Text>Num. de personas: {peopleNumber}</Text>
+        <Text>Precio: {price}</Text>
       </View>
     </ScrollView>
   );
@@ -38,7 +76,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
   },
   carouselWrapper: {
-    height: 300,
+    height: 500,
   },
   slide: {
     flex: 1,
