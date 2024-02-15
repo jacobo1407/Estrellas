7834ae40a5f7f3fb55273d8b8a1a777af682579d
David Roca <114494746+DavidRT6@users.noreply.github.com>
2024-02-15 15:53:34 +0100

    Añadiendo Update y Forgot Password

--- EstrellasApp.js
+++ EstrellasApp.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import {
-  View,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Dimensions,
-  Text,
-} from 'react-native';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -17,6 +10,8 @@ import Rooms from './src/screens/Rooms';
 import Profile from './src/screens/Profile';
 import LogIn from './src/screens/LogIn';
 import SignUp from './src/screens/SignUp';
+import ForgotPassword from './src/screens/ForgotPassword';
+import UpdatePassword from './src/screens/UpdatePassword';
 import Filters from './src/screens/Filters';
 import Payment from './src/screens/Payment';
 import ActiveFilters from './src/screens/ActiveFilters';
@@ -47,6 +42,16 @@ const ProfileStack = () => (
     <Stack.Screen
       name="SignUp"
       component={SignUp}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="ForgotPassword"
+      component={ForgotPassword}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="UpdatePassword"
+      component={UpdatePassword}
       options={{ headerShown: false }}
     />
   </Stack.Navigator>
